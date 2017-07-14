c712fc1543ed6fb7d1cb6dc76989813960499432
Yang <j0238@naver.com>
2017-07-14 17:49:19 +0900

    change search.py and search_log.py to output file

--- search.py
+++ search.py
@@ -1,50 +1,68 @@
 #searching github repositories
 
+from __future__ import print_function
 import requests
 import json
 
-url = 'https://api.github.com/search/'
-search_type = 'issues' #commits, issues etc.
-query_string = '?q=mergeconflict'
-# q : search keywords. able to set keyword with multiple conditions(plain(keyword), repo, language)
-# ex) ?q=pep8+repo:ropas/sparrow+language:c
+rnum = 0
+inum = 100
 
-headers = {'Accept': 'application/vnd.github.v3+json'}
+while(inum>=100):
+    inum = 0
+    rnum += 1
+    url = 'https://api.github.com/search/'
+    search_type = 'issues' #commits, issues etc.
+    query_string = '?q=bug+page='+str(rnum)+'&per_page=100'
+    # q : search keywords. able to set keyword with multiple conditions(plain(keyword), repo, language)
+    # ex) ?q=pep8+repo:ropas/sparrow+language:c
 
-r = requests.get(url+search_type+query_string, headers=headers)
+    headers = {'Accept': 'application/vnd.github.v3+json'}
+    r = requests.get(url+search_type+query_string, headers=headers)
 
-if(r.ok):
-  repoItem = json.loads(r.text or r.content)
+    rst_file = open('result/result'+str(rnum)+'.html','wt')
+    print ('<!DOCTYPE html>', file = rst_file)
+    print ('<html lang="en"><head><meta charset="UTF-8">', file = rst_file)
+    print ('<meta name="viewport" content="width=device-width, initial-scale=1.0">', file = rst_file)
+    print ('<meta http-equiv="X-UA-Compatible" content="ie=edge">', file = rst_file)
+    print ('<title>Result page #'+str(rnum)+'</title></head><body>', file = rst_file)
 
-  if(repoItem['total_count']==0):
-    print 'no result'
-  else:
-    # [print dump]
-    # print json.dumps(repoItem, indent=4, sort_keys=True)
+    if(r.ok):
+      repoItem = json.loads(r.text or r.content)
 
-    # [pretty print result]
-    print 'Result number : '+str(len(repoItem['items']))+'\n'
-    for item in repoItem['items']:
-      print ('issue#'+str(item['number'])+' '+item['title']).encode('utf-8')
-
-      if (item['closed_at']==None):
-        print 'still open'
+      if(repoItem['total_count']==0):
+        print ('no result', file = rst_file)
       else:
-        print 'closed at '+str(item['closed_at'])
+        # [print dump]
+        # print json.dumps(repoItem, indent=4, sort_keys=True)
 
-      print item['html_url']
+        # [pretty print result]
+        inum = len(repoItem['items'])
+        print ('Result number : '+str(inum)+'<br><br>', file = rst_file)
+        for item in repoItem['items']:
+          print (('issuer id : '+item['user']['login']).encode('utf-8'), file = rst_file)
+          print (('issue#'+str(item['number'])+' '+item['title']+' ').encode('utf-8'), file = rst_file)
 
-      if (item['comments']==0):
-        print 'no comments\n'
-      else:
-        print str(item['comments'])+' comments'
-        com_r = requests.get(item['comments_url'], headers=headers)
-        if (com_r.ok):
-          comItem = json.loads(com_r.text or com_r.content)
-          for com in comItem:
-            print (com['user']['login']+' : '+com['body']).encode('utf-8')
-          print '\n'
-        else:\
-          print 'request fail com'
-else:
-  print 'request fail item'
+          if (item['closed_at']==None):
+            print ('still open ', file = rst_file)
+          else:
+            print ('closed at '+str(item['closed_at']+' '), file = rst_file)
+
+          print ('<a href="'+item['html_url']+'">issue link</a><br>', file = rst_file)
+
+          # if (item['comments']==0):
+            # print ('no comments<br><br>', file = rst_file)
+          # else:
+            # print (str(item['comments'])+' comments<br>', file = rst_file)
+            # com_r = requests.get(item['comments_url'], headers=headers)
+            # if (com_r.ok):
+              # comItem = json.loads(com_r.text or com_r.content)
+              # for com in comItem:
+                # print ((com['user']['login']+' : '+com['body']+'<br>').encode('utf-8'), file = rst_file)
+              # print ('<br><br>', file = rst_file)
+            # else:
+              # print ('request fail com', file = rst_file)
+              # comment is hidden b.c. of many lines
+    else:
+      print ('request fail item', file = rst_file)
+
+    print ('</body></html>', file = rst_file)
--- search_log.py
+++ search_log.py
@@ -1,45 +1,62 @@
 #searching github repositories
 
+from __future__ import print_function
 import requests
 import json
 
-url = 'https://api.github.com/search/'
-search_type = 'commits' #commits, issues etc.
-query_string = '?q=semantic error'
-# q : search keywords. able to set keyword with multiple conditions(plain(keyword), repo, language)
-# ex) ?q=pep8+repo:ropas/sparrow+language:c
+rnum = 0
+inum = 100
 
-headers = {'Accept': 'application/vnd.github.cloak-preview'}
+while(inum>=100):
+    inum = 0
+    rnum += 1
+    url = 'https://api.github.com/search/'
+    search_type = 'commits' #commits, issues etc.
+    query_string = '?q=merge bug+Language:c+page='+str(rnum)+'&per_page=100'
+    # q : search keywords. able to set keyword with multiple conditions(plain(keyword), repo, language, pages(default:30))
+    # ex) ?q=pep8+repo:ropas/sparrow+language:c+page=2&per_page=100
 
-r = requests.get(url+search_type+query_string, headers=headers)
+    headers = {'Accept': 'application/vnd.github.cloak-preview'}
+    r = requests.get(url+search_type+query_string, headers=headers)
 
-if(r.ok):
-  repoItem = json.loads(r.text or r.content)
+    rst_file = open('result/result'+str(rnum)+'.html','wt')
+    print ('<!DOCTYPE html>', file = rst_file)
+    print ('<html lang="en"><head><meta charset="UTF-8">', file = rst_file)
+    print ('<meta name="viewport" content="width=device-width, initial-scale=1.0">', file = rst_file)
+    print ('<meta http-equiv="X-UA-Compatible" content="ie=edge">', file = rst_file)
+    print ('<title>Result page #'+str(rnum)+'</title></head><body>', file = rst_file)
 
-  if(repoItem['total_count']==0):
-    print 'no result'
-  else:
-    # [print dump]
-    # print json.dumps(repoItem, indent=4, sort_keys=True)
+    if(r.ok):
+      repoItem = json.loads(r.text or r.content)
 
-    # [pretty print result]
-    print 'Result number : '+str(len(repoItem['items']))+'\n'
-    for item in repoItem['items']:
-      print ('committer id : '+item['commit']['author']['name']).encode('utf-8')
-      print ('commit log : '+item['commit']['message']).encode('utf-8')
-      print ('commit date : '+str(item['commit']['committer']['date'])).encode('utf-8')
-      print item['html_url']
-      if (item['commit']['comment_count']==0):
-        print 'no comments\n'
+      if(repoItem['total_count']==0):
+        print ('no result', file = rst_file)
       else:
-        print str(item['comments']['comment_count'])+' comments'
-        com_r = requests.get(item['comments_url'], headers=headers)
-        if (com_r.ok):
-          comItem = json.loads(com_r.text or com_r.content)
-          for com in comItem:
-            print (com['user']['login']+' : '+com['body']).encode('utf-8')
-          print '\n'
-        else:
-          print 'request fail com'
-else:
-  print 'request fail item'
+        # [print dump]
+        # print json.dumps(repoItem, indent=4, sort_keys=True)
+
+        # [pretty print result]
+        inum = len(repoItem['items'])
+        print ('Result number : '+str(inum)+'<br>', file = rst_file)
+        for item in repoItem['items']:
+          print (('committer id : '+item['commit']['author']['name']).encode('utf-8'), file = rst_file)
+          # print (('commit log : '+item['commit']['message']).encode('utf-8'), file = rst_file)
+          # log is hidden b.c. of many lines
+          print (('commit date : '+str(item['commit']['committer']['date'])).encode('utf-8'), file = rst_file)
+          print ('<a href="'+item['html_url']+'">commit link</a>', file = rst_file)
+          if (item['commit']['comment_count']==0):
+            print ('no comments<br>', file = rst_file)
+          else:
+            print (str(item['comments']['comment_count'])+' comments', file = rst_file)
+            com_r = requests.get(item['comments_url'], headers=headers)
+            if (com_r.ok):
+              comItem = json.loads(com_r.text or com_r.content)
+              for com in comItem:
+                print ((com['user']['login']+' : '+com['body']).encode('utf-8'), file = rst_file)
+              print ('<br>', file = rst_file)
+            else:
+              print ('request fail com', file = rst_file)
+    else:
+      print ('request fail item', file = rst_file)
+
+    print ('</body></html>', file = rst_file)
